c27e556c3b54d0ad3de9f11d48706cca363342da
Suryakant Sharma <suryakant@bureau.id>
2024-09-30 18:21:08 +0530

    Change in xcframework, added error handling

---
+++ iOS SDK project/BureauAuthentication/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  BureauAuthentication
+//
+//  Created by Suryakant Sharma on 26/09/24.
+//  Copyright © 2024 Keyvalue. All rights reserved.
+//
+
+
+import Foundation
+
+public enum AuthenticationStatus {
+    case completed
+    case networkUnavailable
+    case onDifferentNetwork
+    case unknown
+}
