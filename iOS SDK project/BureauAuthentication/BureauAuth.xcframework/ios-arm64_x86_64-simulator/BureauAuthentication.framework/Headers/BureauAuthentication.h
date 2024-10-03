3e1008452d74d0fbb2cfa49d4154d1efc1a1a73f
Suryakant Sharma <suryakant@bureau.id>
2024-10-03 15:56:37 +0530

    Update XCFramework for xcode 16

---
+++ iOS SDK project/BureauAuthentication/BureauAuth.xcframework/ios-arm64_x86_64-simulator/BureauAuthentication.framework/Headers/BureauAuthentication.h
@@ -0,0 +1,20 @@
+//
+//  BureauAuthentication.h
+//  BureauAuthentication
+//
+//  Created by Kurian Ninan K on 09/10/20.
+//  Copyright © 2020 Keyvalue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//#import "HTTPRequester.h"
+
+//! Project version number for BureauAuthentication.
+FOUNDATION_EXPORT double BureauAuthenticationVersionNumber;
+
+//! Project version string for BureauAuthentication.
+FOUNDATION_EXPORT const unsigned char BureauAuthenticationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BureauAuthentication/PublicHeader.h>
+
+
